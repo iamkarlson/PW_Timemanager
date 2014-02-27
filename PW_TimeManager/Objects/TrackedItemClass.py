b994b7f5b8bad581c199caf213356c57938d6289
Georgy Grigoryev <iamkarlson@gmail.com>
2014-02-27 11:28:42 +0400

    mend

--- PW_TimeManager/Objects/TrackedItemClass.py
+++ PW_TimeManager/Objects/TrackedItemClass.py
@@ -1,18 +1,43 @@
 from dotNET_base.NotifyPropertyChangedBase import *
 
+import clr
+import Helpers.clrtype as clrtype
+
+import json
+
 class TrackedItem(NotifyPropertyChangedBase):
+        __metaclass__ = clrtype.ClrClass
         """
                 use setter getter.
                 IronPython 2.6 or later.
         """
+        def __init__(self,type_track,date):
+            super(TrackedItem, self).__init__()
+            self.type_track=type_track
+            self.date=date
+
+        def _jsonSupport( *args ):
+            def default(self, xObject):
+                return { 'type_track':  xObject.type_track, 'date': xObject.date }
+
+            def objectHook( obj ):
+                if 'type' not in obj:
+                    return obj
+                if obj[ 'type' ] != 'TrackedItem':
+                    return obj
+                return TrackedItem(obj.type_track, obj.date )
+            json.JSONEncoder.default = default
+            json._default_decoder = json.JSONDecoder( object_hook = objectHook )
+        _jsonSupport()
+
         @property
-        def type(self):
-                return self._type
+        def type_track(self):
+                return self._type_track
  
-        @type.setter
-        def type(self, value):
-                self._type = value
-                self.OnPropertyChanged("type")
+        @type_track.setter
+        def type_track(self, value):
+                self._type_track = value
+                self.OnPropertyChanged("type_track")
         @property
         def date(self):
                 return self._date
--- PW_TimeManager/PointClass.py
+++
@@ -1,4 +0,0 @@
-class Point(object):
-    def __init__(self,type,date):
-        self.type=type
-        self.date=date
